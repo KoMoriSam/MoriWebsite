a166d20c518ee886169f8a1a2b575ca8573dad9d
KoMoriSam <1760665075@qq.com>
2026-07-20 01:53:41 +0800

    Updates

---
+++ js/Loading-CskUuEnn.js
@@ -0,0 +1,1 @@
+import{c as a,b as n,d as t,E as r}from"./index-1w_j69LY.js";const o={__name:"Loading",props:{size:{type:String,default:""}},setup(s){return(i,e)=>(n(),a("main",{class:r(["flex items-center justify-center",s.size])},e[0]||(e[0]=[t("span",{class:"loading loading-spinner loading-xl text-primary"},null,-1)]),2))}};export{o as _};
