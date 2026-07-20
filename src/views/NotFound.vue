af8cc995be41c63580509031ae516606327305db
KoMoriSam <1760665075@qq.com>
2026-07-20 10:29:14 +0800

    style: 优化 404 页面排版

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,9 +5,26 @@
     title="Ah Oh!"
     description="404 Not Found :("
   >
-    <button class="btn btn-primary mt-2 lg:mt-4" @click="route.push('/')">
-      回到主页
-    </button>
+    <section class="join md:join-vertical mt-2 lg:mt-4">
+      <button
+        class="btn btn-primary btn-soft join-item"
+        @click="route.push('/')"
+      >
+        <i class="ri-home-9-fill"></i>回到主页
+      </button>
+      <button
+        class="btn btn-info btn-soft join-item"
+        @click="route.push('/blog')"
+      >
+        <i class="ri-article-fill"></i>读读文章
+      </button>
+      <button
+        class="btn btn-accent btn-soft join-item"
+        @click="route.push('/novel')"
+      >
+        <i class="ri-book-fill"></i>看看小说
+      </button>
+    </section>
   </BasePage>
 </template>
 
