f49e65ad763b721f086d6d0458d8a46338365293
KoMoriSam <1760665075@qq.com>
2026-07-20 10:32:12 +0800

    refactor(navbar): 更新导航链接以匹配合并后的路由

--- src/components/Layout/NavLinks.vue
+++ src/components/Layout/NavLinks.vue
@@ -5,7 +5,11 @@
       class="btn m-1 lg:m-1.5"
       :class="route.name === link.to.name ? 'btn-primary' : 'btn-ghost'"
     >
-      <i :class="link.icon"></i>
+      <i
+        class="lg:text-xl font-normal"
+        :class="`${link.icon}-${route.name === link.to.name ? 'fill' : 'line'}`"
+      >
+      </i>
       {{ link.name }}
     </router-link>
   </li>
@@ -13,40 +17,31 @@
 
 <script setup>
 import { useRoute } from "vue-router";
-import { useNovelStore } from "@/stores/novelStore";
 
 const route = useRoute();
-
-// 状态管理
-const novelStore = useNovelStore();
 
 import { computed } from "vue";
 
 const navLinks = computed(() => [
   {
     name: "主页",
-    icon: "ri-home-5-line lg:text-xl",
+    icon: "ri-home-9",
     to: { name: "home" },
   },
   {
     name: "博客",
-    icon: "ri-article-line lg:text-xl",
+    icon: "ri-article",
     to: { name: "blog" },
   },
   {
     name: "小说",
-    icon: "ri-book-3-line lg:text-xl",
+    icon: "ri-book-3",
     to: { name: "novel" },
   },
   {
-    name: "联系",
-    icon: "ri-discuss-line lg:text-xl",
-    to: { name: "contact" },
-  },
-  {
-    name: "关于",
-    icon: "ri-contacts-line lg:text-xl",
-    to: { name: "about" },
+    name: "工具",
+    icon: "ri-pencil-ruler-2",
+    to: { name: "tools" },
   },
 ]);
 </script>
